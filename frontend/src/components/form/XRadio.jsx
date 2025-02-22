18f78db624ec4ee4b4ad7d15f3a985e56b4ef7b3
Shahtr1 <tramboos123@gmail.com>
2025-02-22 23:13:53 +0530

    X: Fixed XRadio error color

--- frontend/src/components/form/XRadio.jsx
+++ frontend/src/components/form/XRadio.jsx
@@ -1,11 +1,18 @@
 import { Flex, Radio, Text, useColorModeValue } from "@chakra-ui/react";
 
-export const XRadio = ({ value, label }) => {
+export const XRadio = ({ value, label, hasError }) => {
+  const borderColor = hasError
+    ? useColorModeValue("red.500", "red.300")
+    : useColorModeValue("gray.300", "whiteAlpha.500");
+
+  const borderWidth = hasError ? "2px" : "1px";
+
   return (
     <Flex
       width="100%"
-      border="1px solid"
-      borderColor={useColorModeValue("gray.300", "whiteAlpha.500")}
+      borderWidth={borderWidth}
+      borderStyle="solid"
+      borderColor={borderColor}
       alignItems="center"
       height={{ base: "8", md: "10" }}
       borderRadius="sm"
