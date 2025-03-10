47a17fbb5d0486555e346fc2eed4ccedc1392b21
Shahtr1 <tramboos123@gmail.com>
2025-03-10 20:05:49 +0530

    clean up

--- backend/src/routes/admin.route.ts
+++ backend/src/routes/admin.route.ts
@@ -1,15 +1,7 @@
 import { Router } from "express";
-import { adminDashboardHandler } from "../controllers/admin.controller";
-import {
-  deleteSessionHandler,
-  getSessionsHandler,
-} from "../controllers/session.controller";
 
 const adminRoutes = Router();
 
 // prefix admin
-adminRoutes.get("/dashboard", adminDashboardHandler);
-adminRoutes.get("/sessions", getSessionsHandler);
-adminRoutes.delete("/sessions/:id", deleteSessionHandler);
 
 export default adminRoutes;
