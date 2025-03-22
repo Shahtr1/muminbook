905a151d597947445a1377e651f6c418ae994b71
Shahtr1 <tramboos123@gmail.com>
2025-03-23 04:31:10 +0530

    Added SuhufSVG

--- frontend/src/data/featureItems.js
+++ frontend/src/data/featureItems.js
@@ -1,4 +1,5 @@
 import { FamilyTreeSVG } from "@/components/svgs/FamilyTreeSVG.jsx";
+import { SuhufSVG } from "@/components/svgs/SuhufSVG.jsx";
 
 export const featureItems = () => [
   {
@@ -6,5 +7,11 @@ export const featureItems = () => [
     label: "Family Tree",
     icon: FamilyTreeSVG,
     link: "/features/family-tree",
+  },
+  {
+    id: "suhuf",
+    label: "Suhuf",
+    icon: SuhufSVG,
+    link: "/features/suhuf",
   },
 ];
