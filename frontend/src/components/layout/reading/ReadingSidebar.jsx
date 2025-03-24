0e51f71ca8d0d4528832fdc531531a219787db19
Shahtr1 <tramboos123@gmail.com>
2025-03-24 16:49:17 +0530

    More folder changes

--- frontend/src/components/layout/reading/ReadingSidebar.jsx
+++ frontend/src/components/layout/reading/ReadingSidebar.jsx
@@ -1,9 +1,11 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
 
 export const ReadingSidebar = () => {
   return (
-    <Flex px={8} bgColor="black">
-      Sidebar
+    <Flex px={8} py={4} bgColor="black" color="white" flexDir="column" gap={2}>
+      {Array.from({ length: 50 }, (_, i) => (
+        <Text key={i}>Sidebar {i + 1}</Text>
+      ))}
     </Flex>
   );
 };
