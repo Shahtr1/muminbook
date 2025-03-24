bb0304d3e8b081a41d336a55d7c6e43b682e5dc2
Shahtr1 <tramboos123@gmail.com>
2025-03-24 17:15:17 +0530

    Fixed scroll issues for ReadingSidebar.jsx

--- frontend/src/components/layout/reading/ReadingSidebar.jsx
+++ frontend/src/components/layout/reading/ReadingSidebar.jsx
@@ -1,8 +1,23 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Text, useTheme } from "@chakra-ui/react";
 
 export const ReadingSidebar = () => {
+  const theme = useTheme();
+  const navbarHeight = parseInt(theme.space["navbar-height"]);
+  const breadcrumbHeight = 40;
+
   return (
-    <Flex px={8} py={4} bgColor="black" color="white" flexDir="column" gap={2}>
+    <Flex
+      px={8}
+      py={4}
+      bgColor="black"
+      color="white"
+      flexDir="column"
+      gap={2}
+      h={`calc(100vh - ${navbarHeight + breadcrumbHeight}px)`}
+      overflowY="auto"
+      position="sticky"
+      top={`calc(${navbarHeight + breadcrumbHeight}px)`}
+    >
       {Array.from({ length: 50 }, (_, i) => (
         <Text key={i} whiteSpace="nowrap">
           Sidebar {i + 1}
