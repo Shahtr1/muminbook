4165cc7f92e6848bb6b8198b117d2187f7c1f121
Shahtr1 <tramboos123@gmail.com>
2025-05-09 16:20:12 +0530

    Added surah and ayat svg

--- frontend/src/components/layout/reading/ui/QuranUI.jsx
+++ frontend/src/components/layout/reading/ui/QuranUI.jsx
@@ -1,5 +1,7 @@
 import { RdWrapperUI } from "@/components/layout/reading/ui/RdWrapperUI.jsx";
+import { useColorModeValue } from "@chakra-ui/react";
 
 export const QuranUI = ({ fileId }) => {
+  const frameColor = useColorModeValue("text.primary", "whiteAlpha.900");
   return <RdWrapperUI fileId={fileId}>hi</RdWrapperUI>;
 };
