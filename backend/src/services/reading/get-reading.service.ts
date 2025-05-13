f3c1b8580f6865eaabcb71c6aaace4ecbc443dc1
Shahtr1 <tramboos123@gmail.com>
2025-05-14 05:08:14 +0530

    Fixed loading simple flow

--- backend/src/services/reading/get-reading.service.ts
+++ backend/src/services/reading/get-reading.service.ts
@@ -24,13 +24,13 @@ export const getReading = async (id: string, query?: any) => {
     const mongoQuery: any = {};
     if (surahId) mongoQuery.surahId = new mongoose.Types.ObjectId(surahId);
     if (juzId) mongoQuery.juzId = new mongoose.Types.ObjectId(juzId);
-    if (ruku) mongoQuery.ruku = parseInt(ruku);
-    if (hizbQuarter) mongoQuery.hizbQuarter = parseInt(hizbQuarter);
+    if (ruku != null) mongoQuery.ruku = parseInt(ruku);
+    if (hizbQuarter != null) mongoQuery.hizbQuarter = parseInt(hizbQuarter);
 
     return QuranModel.find(mongoQuery, { createdAt: 0, updatedAt: 0 })
       .sort({ uuid: 1 })
-      .skip(Number(skip))
-      .limit(Number(limit))
+      .skip(parseInt(skip) || 0)
+      .limit(parseInt(limit) || 20)
       .lean();
   } else {
     return await db.collection(id).find({}).sort({ uuid: 1 }).toArray();
