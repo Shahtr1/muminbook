7faf446538be636b76ebde68077131eca24bfbf4
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2026-01-02 03:22:01 +0530

    test: simplify error handling assertions in getDbOrAssertCollection tests

--- packages/backend/src/services/reading/get-reading.service.ts
+++ packages/backend/src/services/reading/get-reading.service.ts
@@ -1,33 +1,7 @@
-import mongoose from 'mongoose';
-import QuranModel from '../../models/quran.model';
-import appAssert from '../../utils/appAssert';
-import {
-  BAD_REQUEST,
-  INTERNAL_SERVER_ERROR,
-  NOT_FOUND,
-} from '../../constants/http';
-
-import SurahModel from '../../models/surah.model';
-import JuzModel from '../../models/juz.model';
-import QuranDivisionType from '../../constants/types/quran/quranDivisionType';
-
-async function getPaginatedCollection(query: any) {
-  console.log('query', query);
-}
+import { getDbOrAssertCollection } from '../../utils/dbHelper';
 
 export const getReading = async (id: string, query: any = {}) => {
-  const db = mongoose.connection.db;
-  appAssert(db, INTERNAL_SERVER_ERROR, 'Database not connected');
+  const db = await getDbOrAssertCollection(id);
 
-  const collections = await db.listCollections().toArray();
-  const collectionExists = collections.some(
-    (col) => col.name.toLowerCase() === id.toLowerCase()
-  );
-  appAssert(collectionExists, NOT_FOUND, `Data of '${id}' not found.`);
-
-  if (id.toLowerCase() !== 'quran') {
-    return await db.collection(id).find({}).sort({ uuid: 1 }).toArray();
-  }
-
-  return await getPaginatedCollection(query);
+  return await db.collection(id).find({}).sort({ uuid: 1 }).toArray();
 };
