d7dedea65f5b9f93bd6f953d11fbfc0cce6548e9
Shahtr1 <tramboos123@gmail.com>
2026-01-30 00:00:34 +0530

    fix: replace per-ayah borders with full-width repeating rule layout

--- packages/frontend/src/components/suhuf/reading/content/QuranContent.jsx
+++ packages/frontend/src/components/suhuf/reading/content/QuranContent.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
-import { Box } from '@chakra-ui/react';
+import { Box, useColorModeValue } from '@chakra-ui/react';
 
 export const QuranContent = ({ data }) => {
+  const ruleColor = useColorModeValue(
+    'rgba(0,0,0,0.15)',
+    'rgba(255,255,255,0.15)'
+  );
+
   return (
     <Box
       fontFamily="ArabicFont"
       textAlign="right"
-      lineHeight="1.8"
+      lineHeight="60px"
       fontSize="35px"
+      wordSpacing="3px"
+      position="relative"
+      backgroundImage={`repeating-linear-gradient(
+        to bottom,
+        transparent 0px,
+        transparent 59px,
+        ${ruleColor} 59px,
+        ${ruleColor} 60px
+      )`}
     >
       {data.map((ayah) => (
-        <span key={ayah._id}>{ayah.ayah} </span>
+        <Box as="span" key={ayah._id}>
+          {/*don't remove empty space after ayah below*/}
+          {ayah.ayah}{' '}
+        </Box>
       ))}
     </Box>
   );
