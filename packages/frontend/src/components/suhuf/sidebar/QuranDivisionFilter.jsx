6902cf433ae346af578afb25e5c4f2c147feebf5
Shahtr1 <tramboos123@gmail.com>
2026-02-04 10:41:57 +0530

    feat: style changes for QuranDivisionFilter.jsx

--- packages/frontend/src/components/suhuf/sidebar/QuranDivisionFilter.jsx
+++ packages/frontend/src/components/suhuf/sidebar/QuranDivisionFilter.jsx
@@ -24,9 +24,11 @@ export const QuranDivisionFilter = ({ value, onChange }) => {
       border="1px solid"
       borderColor={borderColor}
     >
-      <Text fontSize="sm" fontWeight="semibold" mb={3}>
-        Filter By
-      </Text>
+      <Box borderBottom="1px solid" borderColor={borderColor} pb={2} mb={2}>
+        <Text fontSize="sm" fontWeight="semibold">
+          Filter By
+        </Text>
+      </Box>
 
       <RadioGroup value={value} onChange={onChange}>
         <Stack spacing={2} fontSize="sm">
