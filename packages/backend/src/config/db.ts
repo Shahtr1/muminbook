5e2b1d97a86eb4c0d3fc042f4f8a6112b0ce57b6
Shahtr1 <tramboos123@gmail.com>
2026-02-15 09:45:16 +0000

    Fixed db.ts

--- packages/backend/src/config/db.ts
+++ packages/backend/src/config/db.ts
@@ -6,19 +6,7 @@ const connectToDatabase = async () => {
   try {
     log.info('Connecting to MongoDB...');
 
-    const url =
-      'mongodb://shahrukh404:hr8l0v3H%23LL%29W%29RLD@' +
-      'ac-cgn430x-shard-00-00.trqespa.mongodb.net:27017,' +
-      'ac-cgn430x-shard-00-01.trqespa.mongodb.net:27017,' +
-      'ac-cgn430x-shard-00-02.trqespa.mongodb.net:27017/' +
-      'muminbook?' +
-      'replicaSet=atlas-s6g3f4-shard-0&' +
-      'authSource=admin&' +
-      'tls=true&' +
-      'retryWrites=true&' +
-      'w=majority';
-
-    await mongoose.connect(url);
+    await mongoose.connect(MONGO_URI);
 
     log.success('Database connected successfully.');
   } catch (e) {
